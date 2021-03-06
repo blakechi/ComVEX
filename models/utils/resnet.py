61e3c582dfca2ba63c3a898524581364ea5c77e8
blakechi <pwchi@ucdavis.edu>
2021-03-06 04:47:39 -0800

    finish primitive and untested resnet

--- models/utils/resnet.py
+++ models/utils/resnet.py
@@ -2,7 +2,10 @@ import torch
 from torch import nn
 
 
-class ResNetBlock(nn.Module):
+class ResNetBlockBase(nn.Module):
+    """
+    For storing important coefficients arocss different type of ResNet.
+    """
     def __init__(
         self,
         in_channels,
@@ -11,8 +14,7 @@ class ResNetBlock(nn.Module):
         stride=None,
         padding=None,
         norm_layer=None,
-        down_sampling=None
-        ):
+    ):
         super().__init__()
 
         if stride is not None:
@@ -25,7 +27,13 @@ class ResNetBlock(nn.Module):
         self.down_sampling = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 1, self.stride),
             self.norm(out_channels)
-        ) if down_sampling is not None else None
+        ) if self.stride != 1 else None
+
+
+class ResNetBlock(ResNetBlockBase):
+    def __init__(
+        self, in_channels, out_channels, **kwargs):
+        super().__init__(in_channels, out_channels, **kwargs)
 
         self.layers = nn.Sequential(
             nn.Conv2d(
@@ -56,8 +64,8 @@ class ResNetBlock(nn.Module):
 
 
 class ResNetBottleneckBlock(ResNetBlock):
-    def __init__(self, in_channel, out_channel, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, in_channel, out_channel, **kwargs):
+        super().__init__(in_channel, out_channel, **kwargs)
 
         self.layers = nn.Sequential(
             nn.Conv2d(
@@ -88,12 +96,11 @@ class ResNetBottleneckBlock(ResNetBlock):
             self.norm(num_features=out_channels)
         )
         self.relu = nn.ReLU(inplace=True)  # after addition
-        self.down_sampling = down_sampling
 
 
-class FullPreActivationResNetBlock(ResNetBlock):
-    def __init__(self, in_channels, out_channels, *args, **kwargs):
-        super().__init__(in_channels, out_channels, *args, **kwargs)
+class FullPreActivationResNet(ResNetBlockBase):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super().__init__(in_channels, out_channels, **kwargs)
 
         self.layers = nn.Sequential(
             self.norm(out_channels),
@@ -122,5 +129,45 @@ class FullPreActivationResNetBlock(ResNetBlock):
         
         return self.layers(x) + x
 
+
+class FullPreActivationResNetBottleneck(FullPreActivationResNet):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super().__init__(in_channels, out_channels, **kwargs)
+
+        self.layers = nn.Sequential(
+            self.norm(in_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(
+                in_channels,
+                in_channels,
+                1,
+                1,
+                0
+            ),
+            self.norm(in_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(
+                in_channels,
+                in_channels,
+                3,
+                self.stride,
+                self.padding
+            ),
+            self.norm(in_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                1,
+                1,
+                0
+            ),
+        )
+
+
 if __name__ == "__main__":
-    FullPreActivationResNetBlock(10, 10)
+    b = FullPreActivationResNetBottleneck(10, 10)
+
+    a = torch.rand(1, 10, 32, 32)
+
+    print(b(a).size())
