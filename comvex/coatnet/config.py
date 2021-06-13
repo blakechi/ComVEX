c6d2c21422c0c2a788f9a23d4682cdf551364f42
blakechi <pwchi@ucdavis.edu>
2021-06-13 04:40:00 -0700

    CoAtNet - Checked: testing, Remain: example, readme

--- comvex/coatnet/config.py
+++ comvex/coatnet/config.py
@@ -10,9 +10,10 @@ class CoAtNetConfig(ConfigBase):
         image_width: int,
         image_channel: int,
         num_blocks_in_layers: List[int],
-        block_type_in_layers: List[int],
+        block_type_in_layers: List[str],
         num_channels_in_layers: Union[List[int], int],
         expand_scale_in_layers: Union[List[int], int],
+        heads: int,
         num_classes: int,
         pred_act_fnc_name: str = "ReLU",
         ff_dropout: float = 0.,
@@ -27,8 +28,84 @@ class CoAtNetConfig(ConfigBase):
         self.block_type_in_layers = block_type_in_layers
         self.num_channels_in_layers = num_channels_in_layers
         self.expand_scale_in_layers = expand_scale_in_layers
+        self.heads = heads
         self.num_classes = num_classes
         self.pred_act_fnc_name = pred_act_fnc_name
         self.ff_dropout = ff_dropout
         self.attention_dropout = attention_dropout
         self.path_dropout = path_dropout
+
+    @classmethod
+    def CoAtNet_0(cls, num_classes: int, **kwargs) -> 'CoAtNetConfig':
+        return cls(
+            224,
+            224,
+            3,
+            [2, 2, 3, 5, 2],
+            ['C', 'C', 'T', 'T'],
+            [64, 96, 192, 384, 768],
+            4,
+            32,
+            num_classes=num_classes,
+            **kwargs
+        )
+    
+    @classmethod
+    def CoAtNet_1(cls, num_classes: int, **kwargs) -> 'CoAtNetConfig':
+        return cls(
+            224,
+            224,
+            3,
+            [2, 2, 6, 14, 2],
+            ['C', 'C', 'T', 'T'],
+            [64, 96, 192, 384, 768],
+            4,
+            32,
+            num_classes=num_classes,
+            **kwargs
+        )
+
+    @classmethod
+    def CoAtNet_2(cls, num_classes: int, **kwargs) -> 'CoAtNetConfig':
+        return cls(
+            224,
+            224,
+            3,
+            [2, 2, 6, 14, 2],
+            ['C', 'C', 'T', 'T'],
+            [128, 128, 256, 512, 1024],
+            4,
+            32,
+            num_classes=num_classes,
+            **kwargs
+        )
+
+    @classmethod
+    def CoAtNet_3(cls, num_classes: int, **kwargs) -> 'CoAtNetConfig':
+        return cls(
+            224,
+            224,
+            3,
+            [2, 2, 6, 14, 2],
+            ['C', 'C', 'T', 'T'],
+            [192, 192, 384, 768, 1536],
+            4,
+            32,
+            num_classes=num_classes,
+            **kwargs
+        )
+
+    @classmethod
+    def CoAtNet_4(cls, num_classes: int, **kwargs) -> 'CoAtNetConfig':
+        return cls(
+            224,
+            224,
+            3,
+            [2, 2, 12, 28, 2],
+            ['C', 'C', 'T', 'T'],
+            [192, 192, 384, 768, 1536],
+            4,
+            32,
+            num_classes=num_classes,
+            **kwargs
+        )
--- comvex/coatnet/model.py
+++ comvex/coatnet/model.py
@@ -27,10 +27,10 @@ class CoAtNetRelativeAttention(MultiheadAttention):
         self.pre_width = pre_width
 
         self.relative_bias = nn.Parameter(
-            torch.randn(heads, pre_height*pre_width),
+            torch.randn(heads, int(4*(pre_height - 1)*(pre_width - 1))),
             requires_grad=True
         )
-        self.register_buffer["relative_indices"] = self._get_relative_indices(pre_height, pre_width)
+        self.register_buffer("relative_indices", self._get_relative_indices(pre_height, pre_width))
 
     def forward(self, x: torch.tensor) -> torch.tensor:
         b, c, H, W, h = *x.shape, self.heads
@@ -46,7 +46,7 @@ class CoAtNetRelativeAttention(MultiheadAttention):
             relative_bias = self.relative_bias
         else:
             relative_indices = self._get_relative_indices(H, W)
-            relative_bias = nn.functional.interpolate(self.relative_bias, size=(H, W), mode="bilinear")
+            relative_bias = self._interpolate_relative_bias(H, W)
 
         relative_indices = repeat(relative_indices, "n m -> b h n m", b=b, h=h)
         relative_bias = repeat(relative_bias, "h m -> b h n m", b=b, n=H*W)
@@ -66,8 +66,8 @@ class CoAtNetRelativeAttention(MultiheadAttention):
         return out
 
     def _get_relative_indices(self, height: int, width: int) -> torch.tensor:
-        ticks_y = torch.arange(height)
-        ticks_x = torch.arange(width)
+        height, width = int(height), int(width)
+        ticks_y, ticks_x = torch.arange(height), torch.arange(width)
         grid_y, grid_x = torch.meshgrid(ticks_y, ticks_x)
         out = torch.empty(height*width, height*width).fill_(float("nan"))
 
@@ -88,17 +88,19 @@ class CoAtNetRelativeAttention(MultiheadAttention):
 
         return out.to(torch.long)
 
+    def _interpolate_relative_bias(self, height: int, width: int) -> torch.Tensor:
+        out = rearrange(self.relative_bias, "h (n m) -> 1 h n m", n=2*self.pre_height - 1)
+        out = nn.functional.interpolate(out, size=(height, width), mode="bilinear")
+
+        return rearrange(out, "1 h n m -> h (n m)")
+
     def _update_relative_bias_and_indices(self, height: int, width: int) -> None:
         r"""
         For possible input's height or width changes in inference.
         """
 
         self.relative_indices = self._get_relative_indices(height, width)
-        self.relative_bias = nn.functional.interpolate(
-            self.relative_bias,
-            size=(height, width),
-            mode="bilinear"
-        )
+        self.relative_bias = self._interpolate_relative_bias(height, width)
         
 
 class CoAtNetTransformerBlock(nn.Module):
@@ -109,9 +111,11 @@ class CoAtNetTransformerBlock(nn.Module):
         in_dim: int,
         out_dim: int,
         expand_scale: int = 4,
+        use_downsampling: bool = False,
         **kwargs
     ):
         super().__init__()
+        path_dropout = kwargs.pop("path_dropout")
 
         self.norm = nn.Sequential(
             Rearrange("b c h w -> b h w c"),
@@ -125,19 +129,21 @@ class CoAtNetTransformerBlock(nn.Module):
             out_dim,
             **kwargs
         )
-        self.path_dropout = PathDropout(kwargs["path_dropout"] if "path_dropout" in kwargs else 0.)
-        self.pool = nn.AdaptiveMaxPool2d(input_height//2, input_width//2) if in_dim != out_dim else nn.Identity()
-        self.skip = nn.Conv2d(in_dim, out_dim, kernel_size=1) if in_dim != out_dim else nn.Identity()
+        self.attention_path_dropout = PathDropout(path_dropout)
+        self.pool = nn.AdaptiveMaxPool2d((input_height, input_width)) if use_downsampling else nn.Identity()
+        self.skip = nn.Conv2d(in_dim, out_dim, kernel_size=1) if use_downsampling else nn.Identity()
 
         self.ff_block = nn.Sequential(
-            FeedForward(dim=in_dim, ff_dim_scale=expand_scale, **kwargs),
-            PathDropout(kwargs["path_dropout"] if "path_dropout" in kwargs else 0.),
+            nn.Conv2d(out_dim, out_dim*expand_scale, kernel_size=1),
+            nn.GELU(),
+            nn.Conv2d(out_dim*expand_scale, out_dim, kernel_size=1),
         )
+        self.ff_path_dropout = PathDropout(path_dropout)
 
     def forward(self, x):
         # Equation (4) in the official paper with an extra path dropout
-        x = self.skip(self.pool(x)) + self.path_dropout(self.attention_block(self.pool(self.norm(x))))
-        x = x + self.ff_block(x)
+        x = self.skip(self.pool(x)) + self.attention_path_dropout(self.attention_block(self.pool(self.norm(x))))
+        x = x + self.ff_path_dropout(self.ff_block(x))
 
         return x
 
@@ -150,6 +156,7 @@ class CoAtNetConvBlock(nn.Module):
         in_dim: int,
         out_dim: int,
         expand_scale: int = 4,
+        use_downsampling: bool = False,
         **kwargs
     ):
         super().__init__()
@@ -159,15 +166,15 @@ class CoAtNetConvBlock(nn.Module):
             in_dim,
             out_dim,
             expand_scale=expand_scale,
-            first_pixel_wise_conv_stride=2 if in_dim != out_dim else 1,
+            first_pixel_wise_conv_stride=2 if use_downsampling else 1,
             **kwargs
         )
         self.path_dropout = PathDropout(kwargs["path_dropout"] if "path_dropout" in kwargs else 0.)
 
         self.skip = nn.Sequential(
-            nn.AdaptiveMaxPool2d(input_height//2, input_width//2),
+            nn.AdaptiveMaxPool2d((input_height, input_width)),
             nn.Conv2d(in_dim, out_dim, kernel_size=1)
-        ) if in_dim != out_dim else nn.Identity()
+        ) if use_downsampling else nn.Identity()
 
     def forward(self, x):
         # Equation (5) in the offical paper with an extra path dropout
@@ -186,27 +193,29 @@ class CoAtNetBackbone(nn.Module):
         block_type_in_layers: List[int],
         num_channels_in_layers: Union[List[int], int],
         expand_scale_in_layers: Union[List[int], int],
+        heads: int = 32,
         ff_dropout: float = 0.,
         attention_dropout: float = 0.,
         path_dropout: float = 0.,
     ) -> None:
         super().__init__()
 
-        # We ignore `S0` here, so the length of the below lists should be 4
         assert (
-            len(num_blocks_in_layers) == 4
-        ), name_with_msg(self, "The length of `num_blocks_in_layers` must be 4")
-        assert (
-            len(block_type_in_layers) == 4
-        ), name_with_msg(self, "The length of `block_type_in_layers` must be 4")
+            len(num_blocks_in_layers) == 5
+        ), name_with_msg(self, "The length of `num_blocks_in_layers` must be 5")
 
         if isinstance(num_channels_in_layers, list):
             assert (
-                len(num_channels_in_layers) == 4
-            ), name_with_msg(self, "The length of `num_channels_in_layers` must be 4")
+                len(num_channels_in_layers) == 5
+            ), name_with_msg(self, "The length of `num_channels_in_layers` must be 5")
         else:
             begin_channel = int(num_channels_in_layers)
-            num_channels_in_layers = [begin_channel // (2**layer_idx) for layer_idx in range(1, 5)]
+            num_channels_in_layers = [int(begin_channel // (2**layer_idx)) for layer_idx in range(0, 5)]
+
+        # We ignore `S0` here, so the length of the below lists should be 4
+        assert (
+            len(block_type_in_layers) == 4
+        ), name_with_msg(self, "The length of `block_type_in_layers` must be 4")
 
         if isinstance(expand_scale_in_layers, list):
             assert (
@@ -216,52 +225,52 @@ class CoAtNetBackbone(nn.Module):
             expand_scale = int(expand_scale_in_layers)
             expand_scale_in_layers = [expand_scale for _ in range(4)]
 
-        height_in_layers = [image_height / (2**layer_idx) for layer_idx in range(2, 6)]
-        width_in_layers = [image_width / (2**layer_idx) for layer_idx in range(2, 6)]
+        height_in_layers = [int(image_height / (2**layer_idx)) for layer_idx in range(1, 6)]
+        width_in_layers = [int(image_width / (2**layer_idx)) for layer_idx in range(1, 6)]
 
         kwargs = {}
+        kwargs["heads"] = heads
         kwargs["ff_dropout"] = ff_dropout
         kwargs["attention_dropout"] = attention_dropout
         kwargs["path_dropout"] = path_dropout
 
         # Layers
-        self.s_0 = nn.Sequential(
+        self.s_0 = nn.Sequential(*[
             nn.Conv2d(
                 image_channel,
                 num_channels_in_layers[0],
-                kernel=3,
+                kernel_size=3,
                 stride=2,
                 padding=1
-            ),
-            nn.Conv2d(
+            ) if idx == 0 else nn.Conv2d(
                 num_channels_in_layers[0],
                 num_channels_in_layers[0],
-                kernel=3,
+                kernel_size=3,
                 padding=1
-            )
-        )
+            ) for idx in range(num_blocks_in_layers[0])
+        ])
         self.s_1 = self._build_layer("s_1",                                                   # layer name
-            height_in_layers[0], width_in_layers[0],                                          # input size
+            height_in_layers[1], width_in_layers[1],                                          # input size
             num_channels_in_layers[0], num_channels_in_layers[1], expand_scale_in_layers[0],  # dimension-related
-            num_blocks_in_layers[0], block_type_in_layers[0]                                  # block-related
+            num_blocks_in_layers[1], block_type_in_layers[0],                                 # block-related
             **kwargs
         )
         self.s_2 = self._build_layer("s_2",                                                   # layer name
-            height_in_layers[0], width_in_layers[0],                                          # input size
+            height_in_layers[2], width_in_layers[2],                                          # input size
             num_channels_in_layers[1], num_channels_in_layers[2], expand_scale_in_layers[1],  # dimension-related
-            num_blocks_in_layers[1], block_type_in_layers[1]                                  # block-related
+            num_blocks_in_layers[2], block_type_in_layers[1],                                 # block-related
             **kwargs
         )
         self.s_3 = self._build_layer("s_3",                                                   # layer name
-            height_in_layers[0], width_in_layers[0],                                          # input size
+            height_in_layers[3], width_in_layers[3],                                          # input size
             num_channels_in_layers[2], num_channels_in_layers[3], expand_scale_in_layers[2],  # dimension-related
-            num_blocks_in_layers[2], block_type_in_layers[2]                                  # block-related
+            num_blocks_in_layers[3], block_type_in_layers[2],                                 # block-related
             **kwargs
         )
         self.s_4 = self._build_layer("s_4",                                                   # layer name
-            height_in_layers[0], width_in_layers[0],                                          # input size
-            num_channels_in_layers[3], num_channels_in_layers[3], expand_scale_in_layers[3],  # dimension-related
-            num_blocks_in_layers[3], block_type_in_layers[3]                                  # block-related
+            height_in_layers[4], width_in_layers[4],                                          # input size
+            num_channels_in_layers[3], num_channels_in_layers[4], expand_scale_in_layers[3],  # dimension-related
+            num_blocks_in_layers[4], block_type_in_layers[3],                                 # block-related
             **kwargs
         )
 
@@ -299,9 +308,10 @@ class CoAtNetBackbone(nn.Module):
             (f"{layer_name}_{idx}", core_block(
                 input_height=height,
                 input_width=width,
-                in_dim=in_channel,
+                in_dim=in_channel if idx == 0 else out_channel,
                 out_dim=out_channel,
                 expand_scale=expand_scale,
+                use_downsampling=True if idx == 0 else False,
                 **kwargs
             )) for idx in range(num_blocks)
         ]))
@@ -314,7 +324,7 @@ class CoAtNetWithLinearClassifier(CoAtNetBackbone):
         super().__init__(**config.__dict__)
 
         self.proj_head = ProjectionHead(
-            config.dim,
+            config.num_channels_in_layers[-1],
             num_classes,
             pred_act_fnc_name,
         )
