dc2b2593019d1b30b14aada75b07f5e4c2281500
blakechi <pwchi@ucdavis.edu>
2021-06-24 03:51:02 -0700

    RandAugment done except scripting

---
+++ comvex/utils/rand_augment/rand_augment.py
@@ -0,0 +1,79 @@
+from typing import Callable, List, Tuple
+from functools import partial
+
+import torch
+from torch import nn
+try:
+    from typing_extensions import Final
+except:
+    from torch.jit import Final
+
+from .transfrom_functions import (
+    rotate,
+    translate_x,
+    translate_y,
+    sheer_x,
+    sheer_y,
+    color,
+    brightness,
+    sharpness,
+    contrast,
+    auto_contrast,
+    solarize,
+    solarize_add,
+    posterize,
+    equalize,
+    invert,
+    crop_out
+)
+from .config import RandAugmentConfig
+
+
+class RandAugment(nn.Module):
+    n: Final[int]
+    m: Final[int]
+    max_magnitude: Final[int]
+
+    def __init__(
+        self,
+        n: int,
+        m: int,
+        config: RandAugmentConfig = RandAugmentConfig()
+    ) -> None:
+        super().__init__()
+
+        self.n = n
+        self.m = m
+        self.max_magnitude = config.max_magnitude
+        self.register_buffer("transform_func_distribution", torch.tensor(config.transform_func_distribution))
+
+        p = (n - 1) / n  # Since we doesn't put `Identity` in the list
+        self.transform_funcs: Tuple[Callable] = (
+            partial(rotate, p=p, magnitude=self.m, max_magnitude=self.max_magnitude, interval=config.rotate_interval, fill=config.fill),
+            partial(translate_x, p=p, magnitude=self.m, max_magnitude=self.max_magnitude, interval=config.translate_x_interval, fill=config.fill),
+            partial(translate_y, p=p, magnitude=self.m, max_magnitude=self.max_magnitude, interval=config.translate_y_interval, fill=config.fill),
+            partial(sheer_x, p=p, magnitude=self.m, max_magnitude=self.max_magnitude, interval=config.sheer_x_interval, fill=config.fill),
+            partial(sheer_y, p=p, magnitude=self.m, max_magnitude=self.max_magnitude, interval=config.sheer_y_interval, fill=config.fill),
+            partial(color, p=p, magnitude=self.m, max_magnitude=self.max_magnitude),
+            partial(brightness, p=p, magnitude=self.m, max_magnitude=self.max_magnitude),
+            partial(sharpness, p=p, magnitude=self.m, max_magnitude=self.max_magnitude),
+            partial(contrast, p=p, magnitude=self.m, max_magnitude=self.max_magnitude),
+            partial(auto_contrast, p=p),
+            partial(solarize, p=p, magnitude=self.m, max_magnitude=self.max_magnitude),
+            partial(solarize_add, p=p, magnitude=self.m, max_magnitude=self.max_magnitude, addition=config.addition),
+            partial(posterize, p=p, magnitude=self.m, max_magnitude=self.max_magnitude),
+            partial(equalize, p=p),
+            partial(invert, p=p),
+            partial(crop_out, p=p, magnitude=self.m, max_magnitude=self.max_magnitude, interval=config.crop_out_interval, fill=config.fill),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        
+        transform_funcs_indice: List[int] = self.transform_func_distribution.multinomial(num_samples=self.n).tolist()
+        for idx in transform_funcs_indice:
+            x = self.transform_funcs[idx](x)
+
+        return x
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.n}, {self.m})"
--- examples/ConViT/demo.py
+++ examples/ConViT/demo.py
@@ -1,20 +1,26 @@
 import os
 import sys
+
 sys.path.insert(0, os.getcwd())
 
 import torch 
 
 from comvex.convit import ConViTConfig, ConViTWithLinearClassifier
-
+from comvex.utils.rand_augment import RandAugment
 
 if __name__ == "__main__":
 
-    convit_config = ConViTConfig.ConViT_B(num_classes=1000, token_dropout=0.2)
-    convit = ConViTWithLinearClassifier(convit_config)
-
-    print(convit)
-
+    randa = torch.jit.script(RandAugment(3, 5))
     x = torch.randn(1, 3, 224, 224)
+    y = randa(x)
+    print(y)
 
-    print("Input Shape:\n", x.shape)
-    print("Output Shape:\n", convit(x).shape)
+    # convit_config = ConViTConfig.ConViT_B(num_classes=1000, token_dropout=0.2)
+    # convit = ConViTWithLinearClassifier(convit_config)
+
+    # print(convit)
+
+    # x = torch.randn(1, 3, 224, 224)
+
+    # print("Input Shape:\n", x.shape)
+    # print("Output Shape:\n", convit(x).shape)
