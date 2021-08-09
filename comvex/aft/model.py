899dbfdd10ae419d05b12826b8b45ac3ddda3a6e
blakechi <pwchi@ucdavis.edu>
2021-08-09 16:39:16 -0700

    AFT - AFTBackbone

--- comvex/aft/model.py
+++ comvex/aft/model.py
@@ -1,5 +1,6 @@
+from comvex.utils.dropout import PathDropout
 from functools import partial
-from typing import Optional
+from typing import Optional, Literal, Dict
 
 import torch
 from torch import nn, einsum
@@ -10,6 +11,9 @@ except:
     
 from einops import rearrange, repeat
 
+from comvex.vit import ViTBase
+from comvex.cait import ClassAttentionLayer
+from comvex.utils import MLP
 from comvex.utils.helpers.functions import get_act_fnc, name_with_msg
 
 
@@ -103,7 +107,8 @@ class AFTGeneral(nn.Module):
 
         return out
 
-    def _init_weights(self):
+    @torch.jit.ignore
+    def _init_weights(self) -> None:
         if self.use_position_bias:
             nn.init.normal_(self.u, std=0.01)
             nn.init.normal_(self.v, std=0.01)
@@ -111,7 +116,8 @@ class AFTGeneral(nn.Module):
         # other inits...
         
     @staticmethod
-    def get_local_window_mask(local_window_size, max_seq_len):
+    @torch.jit.ignore
+    def get_local_window_mask(local_window_size: int, max_seq_len: int) -> torch.Tensor:
         mask = torch.zeros(max_seq_len, max_seq_len, dtype=torch.bool)
         
         for idx in range(max_seq_len):
@@ -134,22 +140,9 @@ AFTSimple = partial(AFTGeneral, use_position_bias=False)
 AFTLocal = partial(AFTGeneral, use_position_bias=True)
 
 
-# TODO
-class AFTOptimizedLocal(nn.Module):
-    def __init__(
-        self,
-    ) -> None:
-        super().__init__()
-
-
 class AFTDepthWiseConv2DOperator(nn.Module):
-    def __init__(
-        self,
-        dim: int,
-    ) -> None:
+    def __init__(self) -> None:
         super().__init__()
-
-        self.groups = dim
 
     def forward(self, x: torch.Tensor, weight: torch.Tensor) -> torch.Tensor:
         dim = x.shape[-3]
@@ -208,7 +201,7 @@ class AFTConv(nn.Module):
         self._init_weights()
 
     def forward(self, x: torch.Tensor, H: int, W: int) -> torch.Tensor:
-        b, n, d = x
+        # b, n, d = x
 
         #
         q, k, v = self.Q(x), self.K(x), self.V(x)
@@ -238,6 +231,9 @@ class AFTConv(nn.Module):
             outs.append(out)
 
         outs = torch.stack(outs, dim=1)
+        outs = self.attention_dropout(outs)  # temperary implementation
+
+        #
         outs = q*outs
         outs = rearrange(outs, "b p d h w -> b (h w) (p d)")
         outs = self.out_linear(outs)
@@ -245,5 +241,176 @@ class AFTConv(nn.Module):
 
         return outs
 
-    def _init_weights(self):
+    @torch.jit.ignore
+    def _init_weights(self) -> None:
         nn.init.zeros_(self.w_norm.weight)
+
+
+class AFTLayer(nn.Module):
+    r"""Attention Free Transformer Layer for AFT - Full, Simple, Local, Conv or General
+    """
+    
+    MODE_MODULE_MAP: Final[Dict[str, nn.Module]] = {
+        "full": AFTFull,
+        "simple": AFTSimple,
+        "local": AFTLocal,
+        "conv": AFTConv,
+        "general": AFTGeneral,
+    }
+
+    def __init__(
+        self,
+        dim: int,
+        mode: Literal["full", "simple", "local", "conv", "general"] = "full",
+        ff_expand_scale: int = 4,
+        ff_act_fnc_name: str = "GELU",
+        ff_dropout: float = 0.,
+        path_dropout: float = 0.,
+        **kwargs
+    ) -> None:
+        super().__init__()
+        
+        self.use_conv = True if mode == "conv" else False
+
+        self.attn_norm = nn.LayerNorm(dim)
+        self.attn_block = self.MODE_MODULE_MAP[mode](
+            dim=dim,
+            ff_dropout=ff_dropout,
+            **kwargs,
+        )
+        self.attn_path_drop = PathDropout(path_dropout)
+        
+        self.ff_norm = nn.LayerNorm(dim)
+        self.ff_block = MLP(
+            dim=dim,
+            ff_expand_scale=ff_expand_scale,
+            act_fnc_name=ff_act_fnc_name,
+            ff_dropout=ff_dropout
+        )
+        self.ff_path_drop = PathDropout(path_dropout)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        _, _, H, W = x.shape
+        x = rearrange(x, "b c h w -> b (h w) c", h=H, w=W)
+
+        if self.use_conv:
+            x = x + self.attn_path_drop(self.attn_block(self.attn_norm(x), H, W))
+        else:
+            x = x + self.attn_path_drop(self.attn_block(self.attn_norm(x)))
+
+        x = x + self.ff_path_drop(self.ff_block(self.ff_norm(x)))
+
+        x = rearrange(x, "b (h w) c -> b c h w")
+
+        return x
+
+
+class AFTBackbone(ViTBase):
+    r"""Attention Free Transformer Backbone
+
+    - For simplicity and because the strong global interactions from AFT might mitigate the functionality of CLS tokens, 
+      instead of prepending CLS tokens at the beginning, we offer mean pooling or class attention layers from CaiT.
+    """
+
+    use_mean_pooling: Final[bool]
+
+    def __init__(
+        self,
+        image_size: int,
+        image_channel: int,
+        patch_size: int,
+        num_layers: int,
+        dim: int,
+        local_window_size: int,
+        max_seq_len: int,
+        hidden_dim: Optional[int] = None,
+        aft_mode: Literal["full", "simple", "local", "conv", "general"] = "full",
+        pool_mode: Literal["mean", "class"] = "mean",
+        query_act_fnc: str = "Sigmoid",
+        use_bias: bool = False,
+        ff_expand_scale: int = 4,
+        ff_dropout: float = 0.,
+        attention_dropout: float = 0.,
+        path_dropout: float = 0.,
+        # AFT - General, Full, Simple, Local
+        position_bias_dim: int = 128,
+        use_position_bias: bool = True,
+        # AFT - Conv
+        heads: int = 32,
+        epsilon: float = 1e-8,
+        # Possible Class Attention Layer
+        alpha: float = 1e-5,
+        cls_attn_heads: int = 16,
+    ) -> None:
+        super().__init__(image_size, image_channel, patch_size, use_patch_and_flat=True)
+
+        self.patch_proj = nn.Conv2d(image_channel, dim, kernel_size=self.patch_size, stride=self.patch_size, bias=False)
+        
+        self.layers = nn.ModuleList([
+            AFTLayer(
+                dim=dim,
+                mode=aft_mode,
+                local_window_size=local_window_size,
+                max_seq_len=max_seq_len,
+                hidden_dim=hidden_dim,
+                query_act_fnc=query_act_fnc,
+                use_bias=use_bias,
+                heads=heads,
+                epsilon=epsilon,
+                ff_expand_scale=ff_expand_scale,
+                ff_dropout=ff_dropout,
+                path_dropout=path_dropout,
+                attention_dropout=attention_dropout,
+            ) for _ in range(num_layers)
+        ]) if aft_mode == "conv" else nn.ModuleList([
+            AFTLayer(
+                dim=dim,
+                mode=aft_mode,
+                local_window_size=local_window_size,
+                max_seq_len=max_seq_len,
+                hidden_dim=hidden_dim,
+                query_act_fnc=query_act_fnc,
+                use_bias=use_bias,
+                position_bias_dim=position_bias_dim,
+                use_position_bias=use_position_bias,
+                ff_expand_scale=ff_expand_scale,
+                ff_dropout=ff_dropout,
+                path_dropout=path_dropout,
+                attention_dropout=attention_dropout,
+            ) for _ in range(num_layers)
+        ])
+        
+        self.use_mean_pooling = True if pool_mode == "mean" else False
+        if pool_mode == "class":
+            self.CLS = nn.Parameter(torch.randn(1, 1, dim))
+            self.pool = nn.ModuleList([
+                ClassAttentionLayer(
+                    dim=dim,
+                    heads=cls_attn_heads,
+                    alpha=alpha,
+                    ff_expand_scale=ff_expand_scale,
+                    ff_dropout=ff_dropout,
+                    path_dropout=path_dropout,
+                    attention_dropout=attention_dropout,
+                ) for _ in range(2)
+            ])
+        else:
+            self.pool = nn.AdaptiveAvgPool2d(1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        b, *rest = x.shape
+
+        x = self.patch_proj(x)
+
+        for layer in self.layers:
+            x = layer(x)
+
+        if self.use_mean_pooling:
+            out = self.pool(x)
+        else:
+            out = repeat(self.CLS, "1 1 d -> b 1 d", b=b)
+            x = rearrange(x, "b c h w -> b (h w) c")
+            for cls_layer in self.pool:
+                out = cls_layer(out, x)
+
+        return out.view(b, -1)
